cec99b3a8fde1a1cf9aff8002a24d272e73f7732
Andrea <andrea.93@hotmail.it>
2017-08-29 00:10:58 +0200

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,55 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', ['$scope', '$stateParams','$http', '$ionicPopup',
+// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $http, $ionicPopup) {
+     
+    $scope.data={};
+
+        $scope.submit = function () {
+            var link = 'https://eventi-musicali.herokuapp.com/login';
+            email = $scope.data.email;
+            password = $scope.data.password;
+
+            console.log(email, password);
+            
+            $http.post(link, {email : email, password : password}).then(function (res){
+                $scope.response = res.data;
+                console.log(res.data.message);
+                if(res.data.message === undefined){
+                    window.location.href = "#/page4";
+                }
+                else {
+                    var myPopup = $ionicPopup.show({
+                        title: 'ERROR',
+                        subTitle: res.data.message,
+                        buttons: [{text: 'OK', type: 'button-positive'}]                
+                    });
+                } 
+            });
+            
+  }
+
+}])
+   
+.controller('signupCtrl', ['$scope', '$http', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $http, $stateParams) {
+    
+
+}])
+   
+.controller('eventsCtrl', ['$scope', '$http', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $http, $stateParams) {
+    var link = 'https://eventi-musicali.herokuapp.com/events'
+    $http.get(link).then(function (res){
+        $scope.response = res.data;
+    console.log(res.data);
+    });
+}])
+ 
