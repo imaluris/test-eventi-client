6849abdccb4755ee40dc82d14418eafb8616a626
Andrea <andrea.93@hotmail.it>
2017-09-01 12:51:36 +0200

    ulr signup changed

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -34,10 +34,37 @@ function ($scope, $stateParams, $http, $ionicPopup) {
 
 }])
    
-.controller('signupCtrl', ['$scope', '$http', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('signupCtrl', ['$scope', '$http', '$stateParams', '$ionicPopup', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $http, $stateParams) {
+function ($scope, $http, $stateParams, $ionicPopup) {
+
+    $scope.data={};
+    
+            $scope.submit = function () {
+                var link = 'http://eventi-musicali.herokuapp.com/signup';
+                nome = $scope.data.nome;
+                cognome = $scope.data.cognome;
+                email = $scope.data.email;
+                password = $scope.data.password;
+                
+                
+                $http.post(link,{nome : nome, cognome : cognome, email : email, password : password}).then(function (res){
+                    $scope.response = res.data;
+                    console.log(res);
+                    if(res.data.message === undefined){
+                        window.location.href = "#/page4";
+                    }
+                    else {
+                        var myPopup = $ionicPopup.show({
+                            title: 'ERROR',
+                            subTitle: res.data.message,
+                            buttons: [{text: 'OK', type: 'button-positive'}]                
+                        });
+                    } 
+                });
+                
+      }
     
 
 }])
